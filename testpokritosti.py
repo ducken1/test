f02d14da1aefb4c9e9def88d5c566a416f1ff4f7
Luka <luka.lasic123@gmail.com>
2023-05-03 20:40:12 +0200

    deluje?

---
+++ testpokritosti.py
@@ -0,0 +1,10 @@
+# pylint: disable=missing-function-docstring
+"""
+This is a module-level docstring that describes the purpose of the neke module.
+"""
+
+import pytest
+
+if __name__ == '__main__':
+    pytest.main(['C:/Users/duckeN/Desktop/test', '--cov="C:/Users/duckeN/Desktop/test/neke.py"'])
+    
